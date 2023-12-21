b18b3d0b5b3c81c6c0535c9eab78491a9b4352d9
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-12-21 17:21:07 -0500

    update examples and slides

--- test/test_functions.py
+++ test/test_functions.py
@@ -3,12 +3,6 @@ import pytest
 sys.path.append(os.path.join(sys.path[0],'src'))
 from src.functions import add
 
-
-def test_add():
-    assert add(1,1) == 2
-    assert add(1,2) == 3
-
-
 variable_names = "a, b, c"
 values = [(1, 1, 2), 
           (4, 5, 9)]
@@ -18,11 +12,18 @@ def test_parameterized(a, b, c):
     assert add(a,b) == c
 
 
+'''
+def test_add():
+    assert add(1,1) == 2
+    assert add(1,2) == 3
+
 
 @pytest.fixture
 def input_value():
+   # Setup
    input = 39
-   return input
+   yield input
+   # Tear down
 
 def test_divisible_by_3(input_value):
    assert input_value % 3 == 0
@@ -30,3 +31,4 @@ def test_divisible_by_3(input_value):
 
 def test_divisible_by_6(input_value):
    assert input_value % 6 == 0
+'''
